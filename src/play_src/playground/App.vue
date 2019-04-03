de6d9657adafa2b04bb08979cbb5a392d7802746
tsai_ray <simplesmile111@gmail.com>
2019-04-03 18:14:51 +0800

    update practice

--- src/play_src/playground/App.vue
+++ src/play_src/playground/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <DeepInVue />
+    <!-- <DeepInVue /> -->
+    <SlotChildCom>
+      <p slot="title" class="slot_text">{{ slotTitle }}</p>
+      <p >some slot text2</p>
+    </SlotChildCom>
   </div>
 </template>
 
 <script>
-import DeepInVue from './components/DeepInVue.vue';
+// import DeepInVue from './components/DeepInVue.vue';
+import SlotChildCom from './components/SlotChildCom.vue';
 
 export default {
+  data() {
+    return {
+      slotTitle: 'Vue'
+    }
+  },
   components: {
-    DeepInVue
+    // DeepInVue,
+    SlotChildCom
   }
 }
 </script>
 
 <style scoped>
+.slot_text {
+  color: #00f;
+}
 </style>
---
+++ src/play_src/playground/components/SlotChildCom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>this is slot component</p>
+      <slot name="title"></slot>
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scope>
+.slot_text {
+  color: #f00;
+}
+
+</style>
